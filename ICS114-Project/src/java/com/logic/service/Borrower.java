bb82e4544ae264563e4492d2435c8a73d02253da
johngideonsenga <john.xenga@gmail.com>
2018-05-14 10:21:14 +0800

    borrower DB table

---
+++ ICS114-Project/src/java/com/logic/service/Borrower.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.logic.service;
+
+/**
+ *
+ * @author Jonjie
+ */
+public class Borrower {
+    private int borrowerID;
+    private int itemID;
+    private String studentNum;
+    private String lastName;
+    private String firstName;
+    private String section;
+    private String room;
+    private String subject;
+
+    public int getBorrowerID() {
+        return borrowerID;
+    }
+
+    public void setBorrowerID(int borrowerID) {
+        this.borrowerID = borrowerID;
+    }
+
+    public int getItemID() {
+        return itemID;
+    }
+
+    public void setItemID(int itemID) {
+        this.itemID = itemID;
+    }
+
+    public String getStudentNum() {
+        return studentNum;
+    }
+
+    public void setStudentNum(String studentNum) {
+        this.studentNum = studentNum;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSection() {
+        return section;
+    }
+
+    public void setSection(String section) {
+        this.section = section;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    @Override
+    public String toString() {
+        return "Borrower{" + "borrowerID=" + borrowerID + ", itemID=" + itemID + ", studentNum=" + studentNum + ", lastName=" + lastName + ", firstName=" + firstName + ", section=" + section + ", room=" + room + ", subject=" + subject + '}';
+    }
+    
+}
---
+++ ICS114-Project/src/java/com/logic/servlet/borrowServlet.java
@@ -0,0 +1,124 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.logic.servlet;
+
+import com.logic.service.Borrower;
+import com.logic.service.Item;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+/**
+ *
+ * @author Jonjie
+ */
+public class borrowServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            int itemID = Integer.parseInt(request.getParameter("itemID"));
+            String studentNum = request.getParameter("studentNum");
+            String lastName = request.getParameter("lastName");
+            String firstName = request.getParameter("firstName");
+            String section = request.getParameter("section");
+            String room = request.getParameter("room");
+            String subject = request.getParameter("subject");
+            
+            Borrower borrower = new Borrower();
+            borrower.setItemID(itemID);
+            borrower.setStudentNum(studentNum);
+            borrower.setLastName(lastName);
+            borrower.setFirstName(firstName);
+            borrower.setSection(section);
+            borrower.setRoom(room);
+            borrower.setSubject(subject);
+            
+            if(addBorrower(borrower)){
+                out.println("success");
+            }
+            
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+    boolean addBorrower(Borrower borrower){
+        Configuration config = null;
+        SessionFactory factory = null;
+        Session session = null;
+        Transaction transaction = null;
+        
+        try{
+            config = new Configuration();
+            config.configure();
+            factory = config.buildSessionFactory();
+            session = factory.openSession();
+            transaction = session.beginTransaction();            
+            session.save(borrower); // insert
+            transaction.commit();
+            session.close();
+        }
+        catch(Exception e){
+            System.out.println("Exception: "+e);
+            return false;
+        }
+        return true;
+    }
+}
